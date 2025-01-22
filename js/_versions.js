e94d74ceaf10316e018485998015129e2c7d1c51
Ethan Henderson <ethan@zbee.codes>
2025-01-22 10:51:49 -0700

    Bump D4LF Support to 5.8.12

--- js/_versions.js
+++ js/_versions.js
@@ -1,4 +1,4 @@
 const version = '0.0.16';
-const supported_diablo_version = 'v2.0.3';
+const supported_diablo_version = 'v2.1.0';
 const supported_diablo_season = '7';
-const supported_d4lf_version = 'v5.8.10';
+const supported_d4lf_version = 'v5.8.12';
