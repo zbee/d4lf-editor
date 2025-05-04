5ee604dd6c934b6a8b1affe92e356fc24af1a001
Ethan Henderson <ethan@zbee.codes>
2025-05-04 11:56:20 -0600

    Bump D4LF Support to 7.1.13

--- js/_versions.js
+++ js/_versions.js
@@ -1,4 +1,4 @@
 const version = '0.0.17';
 const supported_diablo_version = 'v2.1.0';
-const supported_diablo_season = '7';
-const supported_d4lf_version = 'v5.8.11';
+const supported_diablo_season = '8';
+const supported_d4lf_version = 'v7.1.13';
