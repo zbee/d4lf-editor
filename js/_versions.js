c834b09e96760be9f36b28cc94b29ea660a1f9fe
Ethan Henderson <ethan@zbee.codes>
2025-05-04 12:01:11 -0600

    Bump Diablo Support to 2.2.0

--- js/_versions.js
+++ js/_versions.js
@@ -1,4 +1,4 @@
 const version = '0.0.18';
-const supported_diablo_version = 'v2.1.0';
+const supported_diablo_version = 'v2.2.0';
 const supported_diablo_season = '8';
 const supported_d4lf_version = 'v7.1.13';
